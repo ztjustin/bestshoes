40778d55d0109a130e39431337231f6b1b08e011
Justin Torres <Justin Torres@192.168.1.128>
2017-09-22 02:57:45 -0600

    Init

---
+++ src/main/java/com/bestshoes/BestshoesApplication.java
@@ -0,0 +1,12 @@
+package com.bestshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestshoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestshoesApplication.class, args);
+	}
+}
